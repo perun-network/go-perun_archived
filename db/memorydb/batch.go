bcbd1d794ce9affd012b5a62420a6431774709e0
RmbRT <steffen.rattay@gmail.com>
2019-06-28 12:01:01 +0200

    Changed memorydb.Batch to handle deletions.
    Previously, memorydb.Batch did not handle deletions
    properly. Now, deletions are cached as well.

--- db/memorydb/batch.go
+++ db/memorydb/batch.go
@@ -9,34 +9,38 @@ import (
 )
 
 type Batch struct {
-	db    *Database
-	cache map[string][]byte
-	bytes uint
+	db      *Database
+	writes  map[string][]byte
+	deletes map[string]struct{}
+	bytes   uint
 }
 
 func (this *Batch) Put(key []byte, value []byte) error {
-	oldValue, exists := this.cache[string(key)]
+	skey := string(key)
+	delete(this.deletes, skey)
+	oldValue, exists := this.writes[skey]
 	if exists {
 		this.bytes -= uint(len(oldValue))
 	}
 	this.bytes += uint(len(value))
-	this.cache[string(key)] = value
+	this.writes[skey] = value
 	return nil
 }
 
 func (this *Batch) Delete(key []byte) error {
-	oldValue, exists := this.cache[string(key)]
-	if !exists {
-		return errors.New("Tried to delete nonexistent entry.")
-	} else {
+	skey := string(key)
+	oldValue, exists := this.writes[skey]
+	if exists {
 		this.bytes -= uint(len(oldValue))
-		delete(this.cache, string(key))
-		return nil
+		delete(this.writes, skey)
 	}
+
+	this.deletes[skey] = struct{}{}
+	return nil
 }
 
 func (this *Batch) Len() uint {
-	return uint(len(this.cache))
+	return uint(len(this.writes))
 }
 
 func (this *Batch) ValueSize() uint {
@@ -44,16 +48,24 @@ func (this *Batch) ValueSize() uint {
 }
 
 func (this *Batch) Write() error {
-	for key := range this.cache {
-		err := this.db.Put([]byte(key), this.cache[key])
+	for key := range this.writes {
+		err := this.db.Put([]byte(key), this.writes[key])
 		if err != nil {
 			return errors.Wrap(err, "Failed to put entry.")
+		}
+	}
+
+	for key := range this.deletes {
+		err := this.db.Delete([]byte(key))
+		if err != nil {
+			return errors.Wrap(err, "Failed to delete entry.")
 		}
 	}
 	return nil
 }
 
 func (this *Batch) Reset() {
-	this.cache = nil
+	this.writes = nil
+	this.deletes = nil
 	this.bytes = 0
 }
