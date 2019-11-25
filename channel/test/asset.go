a389a60469bb357ead4e4c375acc7a07cbb9e4a4
Oliver Tale-Yazdi <oliver@perun.network>
2019-11-25 13:58:34 +0100

    :sparkles: [channel/test] Made Asset a wallet.Address

--- channel/test/asset.go
+++ channel/test/asset.go
@@ -8,28 +8,34 @@ import (
 	"io"
 	"math/rand"
 
+	"github.com/pkg/errors"
 	"perun.network/go-perun/channel"
+	"perun.network/go-perun/wallet"
+	wallettest "perun.network/go-perun/wallet/test"
 	"perun.network/go-perun/wire"
 )
 
 // Asset simulates a `perunchannel.Asset` by only containing an `ID`
 type Asset struct {
-	ID int64
+	wallet.Address
 }
 
 var _ channel.Asset = new(Asset)
+var _ wallet.Address = new(Asset)
 
 // newRandomAsset returns a new random Asset
 func newRandomAsset(rng *rand.Rand) *Asset {
-	return &Asset{ID: rng.Int63()}
+	return &Asset{wallettest.NewRandomAddress(rng)}
 }
 
 // Encode encodes an Asset into the io.Writer `w`
-func (a *Asset) Encode(w io.Writer) error {
-	return wire.Encode(w, a.ID)
+func (a Asset) Encode(w io.Writer) error {
+	return wire.Encode(w, a.Address)
 }
 
 // Decode decodes an Asset from the io.Reader `r`
 func (a *Asset) Decode(r io.Reader) error {
-	return wire.Decode(r, &a.ID)
+	add, err := wallet.DecodeAddress(r)
+	*a = Asset{add}
+	return errors.WithMessage(err, "decode asset")
 }
