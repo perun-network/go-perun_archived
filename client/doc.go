1188515ffd870d029235fec60ce1af8faacf0e02
Steffen Rattay <steffen@perun.network>
2020-01-16 19:08:59 +0100

    :bulb: [client] Added doc.go.
    Documented the client package.

---
+++ client/doc.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2019 Chair of Applied Cryptography, Technische Universität
+// Darmstadt, Germany. All rights reserved. This file is part of go-perun. Use
+// of this source code is governed by a MIT-style license that can be found in
+// the LICENSE file.
+
+// The client package contains the Client type.
+// The Client type provides the API that library users are going to use to
+// access the Perun protocol. A client holds a single Perun identity, manages
+// peer connections, as well as all state channels that belong to the client.
+package client
