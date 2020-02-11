fe42ec07516e4ea7662e5a40d6a4fd324e1cd56e
Oliver Tale-Yazdi <oliver@perun.network>
2020-02-11 16:12:06 +0100

    :bulb: [channel] Added documentation

--- channel/errors.go
+++ channel/errors.go
@@ -25,9 +25,9 @@ type (
 	// PhaseTransitionError happens in case of an invalid channel machine phase
 	// transition
 	PhaseTransitionError struct {
-		ID      ID
-		current Phase
-		PhaseTransition
+		ID              ID    // Channel id
+		current         Phase // Phase that the machine was in
+		PhaseTransition       // Attempted transition that was rejected
 	}
 )
 
