e2669a6e2520193c9732eae0a1b2700fe3d433ba
Oliver Tale-Yazdi <oliver@perun.network>
2020-02-11 16:12:15 +0100

    :bulb: [log] Added documentation

--- log/levellified.go
+++ log/levellified.go
@@ -17,10 +17,8 @@ var _ LevelLogger = &Levellified{StdLogger: &log.Logger{}}
 // logger's Print{,f,ln} methods with the prefix [level], except for levels
 // PanicLevel and FatalLevel, which are forwarded to the respective methods.
 type Levellified struct {
-	// wrapped logger
-	StdLogger
-	// Lvl is the current logging level
-	Lvl Level
+	StdLogger       // Wrapped logger
+	Lvl       Level // Current logging level
 }
 
 // Level is the log level.
