5bc723332e34a3d436769aa39be560c1114d755a
Norbert Dzikowski <norbert@perun.network>
2020-08-05 14:02:11 +0200

    :white_check_mark: [eth/channel] Make funder use generic tests
    Signed-off-by: Norbert Dzikowski <norbert@perun.network>

--- backend/ethereum/channel/funder_test.go
+++ backend/ethereum/channel/funder_test.go
@@ -16,9 +16,7 @@ package channel_test
 
 import (
 	"context"
-	"math/big"
 	"math/rand"
-	"sync"
 	"testing"
 	"time"
 
@@ -35,224 +33,66 @@ import (
 	ethwallettest "perun.network/go-perun/backend/ethereum/wallet/test"
 	"perun.network/go-perun/channel"
 	channeltest "perun.network/go-perun/channel/test"
-	pkgtest "perun.network/go-perun/pkg/test"
 	"perun.network/go-perun/wallet"
 	wallettest "perun.network/go-perun/wallet/test"
 )
 
 func TestFunderZeroBalance(t *testing.T) {
-	t.Run("1 Participant", func(t *testing.T) {
-		testFunderZeroBalance(t, 1)
-	})
-	t.Run("2 Participant", func(t *testing.T) {
-		testFunderZeroBalance(t, 2)
-	})
-}
-
-func testFunderZeroBalance(t *testing.T, n int) {
-	rng := pkgtest.Prng(t)
-	parts, funders, params, allocation := newNFunders(context.Background(), t, rng, n)
-
-	for i := range parts {
-		if i%2 == 0 {
-			allocation.Balances[0][i].Set(big.NewInt(0))
-		} // is != 0 otherwise
-		t.Logf("Part: %d ShouldFund: %t Bal: %v", i, i%2 == 1, allocation.Balances[0][i])
-	}
-	// fund
-	var wg sync.WaitGroup
-	wg.Add(n)
-	for i := 0; i < n; i++ {
-		req := channel.FundingReq{
-			Params: params,
-			State:  &channel.State{Allocation: *allocation},
-			Idx:    channel.Index(i),
-		}
-
-		// Check that the funding only changes the nonce when the balance is not zero
-		go func(i int) {
-			defer wg.Done()
-			diff, err := test.NonceDiff(parts[i], funders[i], func() error {
-				return funders[i].Fund(context.Background(), req)
-			})
-			require.NoError(t, err)
-			if i%2 == 0 {
-				assert.Zero(t, diff, "Nonce should stay the same")
-			} else {
-				assert.Equal(t, int(1), diff, "Nonce should increase by 1")
-			}
-		}(i)
-	}
-	wg.Wait()
-	// Check result balances
-	assert.NoError(t, compareOnChainAlloc(params, *allocation, &funders[0].ContractBackend))
+	channeltest.FunderZeroBalance(t, tlfactory{})
 }
 
 func TestFunder_Fund(t *testing.T) {
-	rng := pkgtest.Prng(t)
-	ctx, cancel := context.WithTimeout(context.Background(), defaultTxTimeout)
-	defer cancel()
-	parts, funders, params, allocation := newNFunders(ctx, t, rng, 1)
-	// Test invalid funding request
-	assert.Panics(t, func() { funders[0].Fund(ctx, channel.FundingReq{}) }, "Funding with invalid funding req should fail")
-	// Test funding without assets
-	req := channel.FundingReq{
-		Params: &channel.Params{},
-		State:  &channel.State{},
-		Idx:    0,
-	}
-	assert.NoError(t, funders[0].Fund(ctx, req), "Funding with no assets should succeed")
-	// Test with valid request
-	req = channel.FundingReq{
-		Params: params,
-		State:  &channel.State{Allocation: *allocation},
-		Idx:    0,
-	}
-
-	t.Run("Funding idempotence", func(t *testing.T) {
-		for i := 0; i < 10; i++ {
-			nonce := 0
-			if i == 0 {
-				nonce = 1
-			}
-			diff, err := test.NonceDiff(parts[0], funders[0], func() error {
-				return funders[0].Fund(context.Background(), req)
-			})
-			require.NoError(t, err, "Subsequent Fund calls should not modify the nonce")
-			assert.Equal(t, nonce, diff)
-		}
-	})
-	// Test already closed context
-	cancel()
-	assert.Error(t, funders[0].Fund(ctx, req), "funding with already cancelled context should fail")
-	// Check result balances
-	assert.NoError(t, compareOnChainAlloc(params, *allocation, &funders[0].ContractBackend))
+	channeltest.Funder_Fund(t, tlfactory{})
 }
 
 func TestPeerTimedOutFundingError(t *testing.T) {
-	t.Run("peer 0 faulty out of 2", func(t *testing.T) { testFundingTimeout(t, 0, 2) })
-	t.Run("peer 1 faulty out of 2", func(t *testing.T) { testFundingTimeout(t, 1, 2) })
-	t.Run("peer 0 faulty out of 3", func(t *testing.T) { testFundingTimeout(t, 0, 3) })
-	t.Run("peer 1 faulty out of 3", func(t *testing.T) { testFundingTimeout(t, 1, 3) })
-	t.Run("peer 2 faulty out of 3", func(t *testing.T) { testFundingTimeout(t, 2, 3) })
-}
-
-func testFundingTimeout(t *testing.T, faultyPeer, peers int) {
-	t.Parallel()
-	ctx, cancel := context.WithTimeout(context.Background(), defaultTxTimeout)
-	defer cancel()
-	rng := pkgtest.Prng(t)
-	ct := pkgtest.NewConcurrent(t)
-
-	_, funders, params, allocation := newNFunders(ctx, t, rng, peers)
-
-	for i, funder := range funders {
-		sleepTime := time.Duration(rng.Int63n(10) + 1)
-		i, funder := i, funder
-		go ct.StageN("funding loop", peers, func(rt require.TestingT) {
-			// Faulty peer does not fund the channel.
-			if i == faultyPeer {
-				return
-			}
-			time.Sleep(sleepTime * time.Millisecond)
-			req := channel.FundingReq{
-				Params: params,
-				State:  &channel.State{Allocation: *allocation},
-				Idx:    uint16(i),
-			}
-			defer cancel()
-			err := funder.Fund(ctx, req)
-			require.True(rt, channel.IsFundingTimeoutError(err), "funder should return FundingTimeoutError")
-			pErr := errors.Cause(err).(*channel.FundingTimeoutError) // unwrap error
-			assert.Equal(t, pErr.Errors[0].Asset, 0, "Wrong asset set")
-			assert.Equal(t, uint16(faultyPeer), pErr.Errors[0].TimedOutPeers[0], "Peer should be detected as erroneous")
-		})
-	}
-
-	time.Sleep(100 * time.Millisecond) // give all funders enough time to fund
-	// Hackily extract SimulatedBackend from funder
-	sb, ok := funders[0].ContractInterface.(*test.SimulatedBackend)
-	require.True(t, ok)
-	// advance block time so that funding fails for non-funders
-	require.NoError(t, sb.AdjustTime(time.Duration(params.ChallengeDuration)*time.Second))
-	sb.Commit()
-
-	ct.Wait("funding loop")
+	channeltest.PeerTimedOutFundingError(t, tlfactory{})
 }
 
 func TestFunder_Fund_multi(t *testing.T) {
-	t.Run("1-party funding", func(t *testing.T) { testFunderFunding(t, 1) })
-	t.Run("2-party funding", func(t *testing.T) { testFunderFunding(t, 2) })
-	t.Run("3-party funding", func(t *testing.T) { testFunderFunding(t, 3) })
-	t.Run("10-party funding", func(t *testing.T) { testFunderFunding(t, 10) })
+	channeltest.Funder_Fund_Multi(t, tlfactory{})
 }
 
-func testFunderFunding(t *testing.T, n int) {
-	ctx, cancel := context.WithTimeout(context.Background(), defaultTxTimeout)
-	defer cancel()
-	rng := pkgtest.Prng(t)
-	ct := pkgtest.NewConcurrent(t)
+var _ channeltest.TestableLedger = new(uniformLedger)
 
-	_, funders, params, allocation := newNFunders(ctx, t, rng, n)
+// uniformLedger wraps a backend, in this case `test.SimulatedBackend` and implements
+// the `channeltest.TestableLedger` interface to allow testing via generic tests.
+type uniformLedger struct {
+	simBackend *test.SimulatedBackend
+}
 
-	for i, funder := range funders {
-		sleepTime := time.Duration(rng.Int63n(10) + 1)
-		i, funder := i, funder
-		go ct.StageN("funding", n, func(rt require.TestingT) {
-			time.Sleep(sleepTime * time.Millisecond)
-			req := channel.FundingReq{
-				Params: params,
-				State:  &channel.State{Allocation: *allocation},
-				Idx:    channel.Index(i),
-			}
-			err := funder.Fund(ctx, req)
-			require.NoError(rt, err, "funding should succeed")
-		})
+type tlfactory struct{}
+
+// TLFactory builds a distinct `TestableLedger` instance which can be used in
+// subsequent tests.
+func (tlf tlfactory) Build() channeltest.TestableLedger {
+	return new(uniformLedger)
+}
+
+// PostFundingCheck is a test which manually calls `funder.Fund(...)` and
+// asserts that ledger-specific postconditions are satisfied. E.g. nonce checks.
+func (ul *uniformLedger) PostFundingCheck(t *testing.T,
+	addr wallet.Address,
+	funder channel.Funder,
+	req channel.FundingReq,
+	success bool) {
+	diff, err := test.NonceDiff(addr, funder.(*ethchannel.Funder), func() error {
+		return funder.Fund(context.Background(), req)
+	})
+	require.NoError(t, err)
+	if success {
+		assert.Equal(t, int(1), diff, "Nonce should increase by 1")
+	} else {
+		assert.Zero(t, diff, "Nonce should stay the same")
 	}
-
-	ct.Wait("funding")
-	// Check result balances
-	assert.NoError(t, compareOnChainAlloc(params, *allocation, &funders[0].ContractBackend))
 }
 
-func newNFunders(
-	ctx context.Context,
-	t *testing.T,
-	rng *rand.Rand,
-	n int,
-) (
-	parts []wallet.Address,
-	funders []*ethchannel.Funder,
-	params *channel.Params,
-	allocation *channel.Allocation,
-) {
-	simBackend := test.NewSimulatedBackend()
-	ks := ethwallettest.GetKeystore()
-	deployAccount := &wallettest.NewRandomAccount(rng).(*ethwallet.Account).Account
-	simBackend.FundAddress(ctx, deployAccount.Address)
-	contractBackend := ethchannel.NewContractBackend(simBackend, ks, deployAccount)
-	// Deploy Assetholder
-	assetETH, err := ethchannel.DeployETHAssetholder(ctx, contractBackend, deployAccount.Address)
-	require.NoError(t, err, "Deployment should succeed")
-	t.Logf("asset holder address is %v", assetETH)
-	parts = make([]wallet.Address, n)
-	funders = make([]*ethchannel.Funder, n)
-	for i := 0; i < n; i++ {
-		acc := wallettest.NewRandomAccount(rng).(*ethwallet.Account)
-		simBackend.FundAddress(ctx, acc.Account.Address)
-		parts[i] = acc.Address()
-		cb := ethchannel.NewContractBackend(simBackend, ks, &acc.Account)
-		funders[i] = ethchannel.NewETHFunder(cb, assetETH)
-	}
-	// The SimBackend advances 10 sec per transaction/block, so generously add 20
-	// sec funding duration per participant
-	params = channeltest.NewRandomParams(rng, channeltest.WithParts(parts...), channeltest.WithChallengeDuration(uint64(n)*20))
-	allocation = channeltest.NewRandomAllocation(rng, channeltest.WithNumParts(n), channeltest.WithAssets((*ethchannel.Asset)(&assetETH)))
-	return
-}
-
-// compareOnChainAlloc returns error if `alloc` differs from the on-chain allocation.
-func compareOnChainAlloc(params *channel.Params, alloc channel.Allocation, cb *ethchannel.ContractBackend) error {
+// CompareOnChainAlloc compares the allocations for all accounts and assets
+// onchain to the given allocation.
+func (ul *uniformLedger) CompareOnChainAlloc(params *channel.Params,
+	alloc channel.Allocation,
+	funder channel.Funder) error {
+	cb := &funder.(*ethchannel.Funder).ContractBackend
 	onChain, err := getOnChainAllocation(context.Background(), cb, params, alloc.Assets)
 	if err != nil {
 		return errors.WithMessage(err, "getting on-chain allocation")
@@ -260,14 +100,69 @@ func compareOnChainAlloc(params *channel.Params, alloc channel.Allocation, cb *e
 	for a := range onChain {
 		for p := range onChain[a] {
 			if alloc.Balances[a][p].Cmp(onChain[a][p]) != 0 {
-				return errors.Errorf("balances[%d][%d] differ. Expected: %v, on-chain: %v", a, p, alloc.Balances[a][p], onChain[a][p])
+				return errors.Errorf("balances[%d][%d] differ. Expected: %v, on-chain: %v",
+					a, p, alloc.Balances[a][p], onChain[a][p])
 			}
 		}
 	}
 	return nil
 }
 
-func getOnChainAllocation(ctx context.Context, cb *ethchannel.ContractBackend, params *channel.Params, _assets []channel.Asset) ([][]channel.Bal, error) {
+// AdvanceTimeBy advances the time for the underlying Ledger.
+func (ul *uniformLedger) AdvanceTimeBy(ctx context.CancelFunc, t *testing.T, challengeDuration uint64) {
+	// advance block time so that funding fails for non-funders
+	require.NoError(t, ul.simBackend.AdjustTime(time.Duration(challengeDuration)*time.Second))
+	ul.simBackend.Commit()
+}
+
+// NewNFunders creates a new prefunded blockchain for N participants and
+// appropriate funds. `allocation` is the allocation which should be used
+// in a `channel.fundingReq` to fund a new channel. Using this allocation
+// is guaranteed to result in a successful funding.
+func (ul *uniformLedger) NewNFunders(
+	ctx context.Context,
+	t *testing.T,
+	rng *rand.Rand,
+	n int,
+) (
+	parts []wallet.Address,
+	funders []channel.Funder,
+	params *channel.Params,
+	allocation *channel.Allocation,
+) {
+	ul.simBackend = test.NewSimulatedBackend()
+	ks := ethwallettest.GetKeystore()
+	deployAccount := &wallettest.NewRandomAccount(rng).(*ethwallet.Account).Account
+	ul.simBackend.FundAddress(ctx, deployAccount.Address)
+	contractBackend := ethchannel.NewContractBackend(ul.simBackend, ks, deployAccount)
+	// Deploy Assetholder
+	assetETH, err := ethchannel.DeployETHAssetholder(ctx, contractBackend, deployAccount.Address)
+	require.NoError(t, err, "Deployment should succeed")
+	t.Logf("asset holder address is %v", assetETH)
+	parts = make([]wallet.Address, n)
+	funders = make([]channel.Funder, n)
+	for i := 0; i < n; i++ {
+		acc := wallettest.NewRandomAccount(rng).(*ethwallet.Account)
+		ul.simBackend.FundAddress(ctx, acc.Account.Address)
+		parts[i] = acc.Address()
+		cb := ethchannel.NewContractBackend(ul.simBackend, ks, &acc.Account)
+		funders[i] = ethchannel.NewETHFunder(cb, assetETH)
+	}
+	// The SimBackend advances 10 sec per transaction/block, so generously add 20
+	// sec funding duration per participant
+	params = channeltest.NewRandomParams(rng,
+		channeltest.WithParts(parts...),
+		channeltest.WithChallengeDuration(uint64(n)*20))
+	allocation = channeltest.NewRandomAllocation(rng,
+		channeltest.WithNumParts(len(parts)),
+		channeltest.WithAssets((*ethchannel.Asset)(&assetETH)))
+	return
+}
+
+func getOnChainAllocation(ctx context.Context,
+	cb *ethchannel.ContractBackend,
+	params *channel.Params,
+	_assets []channel.Asset) ([][]channel.Bal, error) {
 	partIDs := ethchannel.FundingIDs(params.ID(), params.Parts...)
 	alloc := make([][]channel.Bal, len(_assets))
 
