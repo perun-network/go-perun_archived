e6794a3a819c857ea3bc4ff7527db80e3efa2dde
Sebastian Stammler <seb@perun.network>
2020-11-09 11:35:02 +0100

    :sparkles: [eth/channel] Add AssetHolderERC20 validator.
    Code duplication with the ETH asset holder functions eliminated by using
    a common `validateAssetHolder` function and common test logic.
    
    Signed-off-by: Sebastian Stammler <seb@perun.network>

--- backend/ethereum/channel/asset_test.go
+++ backend/ethereum/channel/asset_test.go
@@ -18,6 +18,8 @@ import (
 	"context"
 	"testing"
 
+	"github.com/ethereum/go-ethereum/accounts"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/stretchr/testify/require"
 
@@ -28,8 +30,31 @@ import (
 )
 
 func TestValidateAssetHolderETH(t *testing.T) {
-	// Test setup
+	testValidateAssetHolder(t, ethchannel.DeployETHAssetholder, ethchannel.ValidateAssetHolderETH)
+}
+
+func TestValidateAssetHolderERC20(t *testing.T) {
 	rng := pkgtest.Prng(t)
+	token := common.Address(ethwallettest.NewRandomAddress(rng))
+	deployer := func(ctx context.Context,
+		backend ethchannel.ContractBackend,
+		adjudicatorAddr common.Address,
+		deployer accounts.Account) (common.Address, error) {
+		return ethchannel.DeployERC20Assetholder(ctx, backend, adjudicatorAddr, token, deployer)
+	}
+
+	testValidateAssetHolder(t, deployer, ethchannel.ValidateAssetHolderERC20)
+}
+
+func testValidateAssetHolder(t *testing.T,
+	deployer func(ctx context.Context,
+		backend ethchannel.ContractBackend,
+		adjudicatorAddr common.Address,
+		deployer accounts.Account) (common.Address, error),
+	validator func(ctx context.Context,
+		backend bind.ContractBackend, assetHolderETH, adjudicator common.Address) error) {
+	// Test setup
+	rng := pkgtest.Prng(t, "validate")
 	s := test.NewSimSetup(rng)
 
 	t.Run("no_asset_code", func(t *testing.T) {
@@ -37,35 +62,38 @@ func TestValidateAssetHolderETH(t *testing.T) {
 		randomAddr2 := (common.Address)(ethwallettest.NewRandomAddress(rng))
 		ctx, cancel := context.WithTimeout(context.Background(), defaultTxTimeout)
 		defer cancel()
-		require.True(t, ethchannel.IsErrInvalidContractCode(ethchannel.ValidateAssetHolderETH(ctx, *s.CB, randomAddr1, randomAddr2)))
+		require.True(t, ethchannel.IsErrInvalidContractCode(validator(ctx, s.CB, randomAddr1, randomAddr2)))
 	})
+
 	t.Run("incorrect_asset_code", func(t *testing.T) {
 		randomAddr1 := (common.Address)(ethwallettest.NewRandomAddress(rng))
 		ctx, cancel := context.WithTimeout(context.Background(), defaultTxTimeout)
 		defer cancel()
 		incorrectCodeAddr, err := ethchannel.DeployAdjudicator(ctx, *s.CB, s.TxSender.Account)
 		require.NoError(t, err)
-		require.True(t, ethchannel.IsErrInvalidContractCode(ethchannel.ValidateAssetHolderETH(ctx, *s.CB, incorrectCodeAddr, randomAddr1)))
+		require.True(t, ethchannel.IsErrInvalidContractCode(validator(ctx, s.CB, incorrectCodeAddr, randomAddr1)))
 	})
+
 	t.Run("incorrect_adj_addr", func(t *testing.T) {
 		adjAddrToSet := (common.Address)(ethwallettest.NewRandomAddress(rng))
 		adjAddrToExpect := (common.Address)(ethwallettest.NewRandomAddress(rng))
 		ctx, cancel := context.WithTimeout(context.Background(), defaultTxTimeout)
 		defer cancel()
-		assetHolderAddr, err := ethchannel.DeployETHAssetholder(ctx, *s.CB, adjAddrToSet, s.TxSender.Account)
+		assetHolderAddr, err := deployer(ctx, *s.CB, adjAddrToSet, s.TxSender.Account)
 		require.NoError(t, err)
 		t.Logf("assetholder address is %v", assetHolderAddr)
-		require.True(t, ethchannel.IsErrInvalidContractCode(ethchannel.ValidateAssetHolderETH(ctx, *s.CB, assetHolderAddr, adjAddrToExpect)))
+		require.True(t, ethchannel.IsErrInvalidContractCode(validator(ctx, s.CB, assetHolderAddr, adjAddrToExpect)))
 	})
+
 	t.Run("all_correct", func(t *testing.T) {
 		ctx, cancel := context.WithTimeout(context.Background(), defaultTxTimeout)
 		defer cancel()
 		adjudicatorAddr, err := ethchannel.DeployAdjudicator(ctx, *s.CB, s.TxSender.Account)
 		require.NoError(t, err)
-		assetHolderAddr, err := ethchannel.DeployETHAssetholder(ctx, *s.CB, adjudicatorAddr, s.TxSender.Account)
+		assetHolderAddr, err := deployer(ctx, *s.CB, adjudicatorAddr, s.TxSender.Account)
 		require.NoError(t, err)
 		t.Logf("adjudicator address is %v", adjudicatorAddr)
 		t.Logf("assetholder address is %v", assetHolderAddr)
-		require.NoError(t, ethchannel.ValidateAssetHolderETH(ctx, *s.CB, assetHolderAddr, adjudicatorAddr))
+		require.NoError(t, validator(ctx, s.CB, assetHolderAddr, adjudicatorAddr))
 	})
 }
