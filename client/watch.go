2c698a1f83a74bc8ab262772dd5be0c9c2819775
Matthias Geihs <matthias@perun.network>
2020-11-12 16:01:16 +0100

    :construction: [client] Revise watcher and settle for on-chain progression

--- client/watch.go
+++ client/watch.go
@@ -22,150 +22,143 @@ import (
 	"perun.network/go-perun/channel"
 )
 
-// Watch starts the channel watcher routine. It subscribes to RegisteredEvents
-// on the adjudicator. If an event is registered, it is handled by making sure
-// the latest state is registered and then all funds withdrawn to the receiver
-// specified in the adjudicator that was passed to the channel.
-//
-// If handling failed, the watcher routine returns the respective error. It is
-// the user's job to restart the watcher after the cause of the error got fixed.
-func (c *Channel) Watch() error {
+// WatchEventHandler represents an interface for handling adjudicator events.
+type WatchEventHandler interface {
+	HandleRegistered(*channel.RegisteredEvent)
+	HandleProgressed(*channel.ProgressedEvent)
+}
+
+// Watch watches the adjudicator for channel events and responds accordingly.
+func (c *Channel) Watch(h WatchEventHandler) error {
 	log := c.Log().WithField("proc", "watcher")
 	defer log.Info("Watcher returned.")
 
+	// Subscribe to state changes
 	ctx := c.Ctx()
-	sub, err := c.adjudicator.SubscribeRegistered(ctx, c.Params())
+	sub, err := c.adjudicator.Subscribe(ctx, c.Params())
 	if err != nil {
-		return errors.WithMessage(err, "subscribing to RegisteredEvents")
+		return errors.WithMessage(err, "subscribing to adjudicator state changes")
 	}
 	// nolint:errcheck
 	defer sub.Close()
 	// nolint:errcheck,gosec
 	c.OnCloseAlways(func() { sub.Close() })
 
-	// Wait for on-chain event
-	reg := sub.Next()
-	log.Infof("New RegisteredEvent: %v", reg)
-	if reg == nil {
-		err := sub.Err() // err might be nil if subscription got orderly closed
-		log.Debugf("Subscription closed: %v", err)
+	// Wait for state changed event
+	for e := sub.Next(); e != nil; e = sub.Next() {
+		log.Infof("event %v", e)
+
+		switch e := e.(type) {
+		case *channel.RegisteredEvent:
+			// Assert backend version not greater than local version.
+			if e.Version > c.machine.State().Version {
+				// If the implementation works as intended, this should never happen.
+				log.Panicf("watch: registered: expected version less than or equal to %d, got version %d", c.machine.State().Version, e.Version)
+			}
+
+			// If local version greater than backend version, register local state.
+			if e.Version < c.machine.State().Version {
+				if err := c.Register(ctx); err != nil {
+					return errors.WithMessage(err, "registering")
+				}
+			}
+
+			go h.HandleRegistered(e)
+
+		case *channel.ProgressedEvent:
+			//todo: lock machine!
+			c.machine.SetProgressed(e)
+			go h.HandleProgressed(e)
+
+		}
+	}
+
+	log.Debugf("Subscription closed: %v", sub.Err())
+
+	if err := sub.Err(); err != nil {
 		return errors.WithMessage(err, "subscription closed")
 	}
 
-	return errors.WithMessage(
-		c.handleRegisteredEvent(ctx, reg),
-		"handling RegisteredEvent")
+	return nil
 }
 
-// handleRegisteredEvent stores the passed RegisteredEvent to the machine and
-// settles the channel.
-func (c *Channel) handleRegisteredEvent(ctx context.Context, reg *channel.RegisteredEvent) error {
-	log := c.Log().WithField("proc", "watcher")
-	// Lock machine while registering is in progress.
+// Register registers the channel on the adjudicator.
+func (c *Channel) Register(ctx context.Context) error {
+	// Lock channel machine.
+	if !c.machMtx.TryLockCtx(ctx) {
+		return errors.WithMessage(ctx.Err(), "locking machine")
+	}
+	defer c.machMtx.Unlock()
+
+	return c.register(ctx)
+}
+
+// ProgressBy progresses the channel state in the adjudicator backend.
+func (c *Channel) ProgressBy(ctx context.Context, update func(*channel.State)) error {
+	// Lock machine
 	if !c.machMtx.TryLockCtx(ctx) {
 		return errors.Errorf("locking machine mutex in time: %v", ctx.Err())
 	}
 	defer c.machMtx.Unlock()
 
-	if c.machine.Phase() == channel.Withdrawn {
-		// If a Settle call by the user caused this event, the channel will be
-		// withdrawn already and we're done.
-		log.Debug("Channel already withdrawn.")
-		return nil
+	// Store current state
+	ar := c.machine.AdjudicatorReq()
+
+	// Update state
+	state := c.machine.State().Clone()
+	state.Version++
+	update(state)
+
+	// Apply state in machine and generate signature
+	c.machine.SetProgressing(state)
+	sig, err := c.machine.Sig(ctx)
+	if err != nil {
+		return errors.WithMessage(err, "signing")
 	}
 
-	if err := c.machine.SetRegistered(ctx, reg); err != nil {
-		return errors.WithMessage(err, "setting machine to Registered phase")
+	// Create and send request
+	pr := channel.MakeProgressRequest(ar, state, sig)
+	if err := c.adjudicator.Progress(ctx, pr); err != nil {
+		return errors.WithMessage(err, "progressing")
 	}
 
-	return c.settle(ctx, false)
+	return nil
 }
 
-// Settle settles the channel: it is made sure that the current state is
-// registered and the final balance withdrawn. This call blocks until the
-// channel has been successfully withdrawn.
-func (c *Channel) Settle(ctx context.Context) error {
-	return c.settleLocked(ctx, false)
-}
-
-// SettleSecondary settles the channel: it is made sure that the current state
-// is registered and the final balance withdrawn. This call blocks until the
-// channel has been successfully withdrawn.
-//
-// SettleSecondary is a variant of Settle that can be called when
-// collaboratively settling a channel at the same time as the other channel
-// peers. The initiator of the channel settlement should call Settle, whereas
-// all responders can call SettleSecondary. The blockchain backend might then
-// run an optimized settlement protocol that possibly saves sending unnecessary
-// duplicate transactions in parallel. If the initiator is maliciously not
-// sending the required transactions, the backend guarantees that it will
-// eventually send the required transactions, so it is always safe to call
-// SettleSecondary.
-func (c *Channel) SettleSecondary(ctx context.Context) error {
-	return c.settleLocked(ctx, true)
-}
-
-// settleLocked calls settle with the channel mutex locked and the context also
-// set to cancel when the client is closed.
-func (c *Channel) settleLocked(ctx context.Context, secondary bool) error {
+// Withdraw concludes a registered channel and withdraws the funds.
+func (c *Channel) Withdraw(ctx context.Context, subStates map[channel.ID]*channel.State, secondary bool) error {
+	// Lock channel machine.
 	if !c.machMtx.TryLockCtx(ctx) {
-		return errors.Errorf("locking machine mutex in time: %v", ctx.Err())
+		return errors.WithMessage(ctx.Err(), "locking machine")
 	}
 	defer c.machMtx.Unlock()
-	// Wrap the context to make sure that the settle call stops as soon as the
-	// channel controller is closed.
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-	c.OnClose(cancel)
-	return c.settle(ctx, secondary)
-}
 
-// settle makes sure that the current state is registered and the final balance
-// withdrawn. This call blocks until the channel has been successfully
-// withdrawn.
-//
-// If the secondary flag is true and the channel is in a final state, the
-// blockchain backend might run an optimized withdrawing protocol, possibly
-// skipping the sending of unnecessary transactions, where the other user is
-// assumed to be the initiator of the settlement process.
-//
-// The caller is expected to have locked the channel mutex.
-func (c *Channel) settle(ctx context.Context, secondary bool) error {
-	if c.IsSubChannel() {
-		return c.subChannelSettleOptimistic(ctx)
-	}
-	ver, reg := c.machine.State().Version, c.machine.Registered()
-	// If the machine is at least in phase Registered, reg shouldn't be nil. We
-	// still catch this case to be future proof.
-	if c.machine.Phase() < channel.Registered || reg == nil || reg.Version < ver {
-		if reg != nil && reg.Version < ver {
-			c.Log().Warnf("Lower version %d (< %d) registered, refuting...", reg.Version, ver)
-		}
-		if err := c.register(ctx); err != nil {
-			return errors.WithMessage(err, "registering")
-		}
-		c.Log().Info("Channel state registered.")
+	if err := c.machine.SetWithdrawing(ctx); err != nil {
+		return errors.WithMessage(err, "setting machine to withdrawing phase")
 	}
 
-	if reg = c.machine.Registered(); !reg.Timeout.IsElapsed(ctx) {
-		if c.machine.State().IsFinal {
-			c.Log().Warnf(
-				"Unexpected withdrawal timeout while settling final state. Waiting until %v.",
-				reg.Timeout)
-		} else {
-			c.Log().Infof("Waiting until %v for withdrawal.", reg.Timeout)
+	switch {
+	case c.IsLedgerChannel():
+		req := c.machine.AdjudicatorReq()
+		req.Secondary = secondary
+		if err := c.adjudicator.Withdraw(ctx, req, subStates); err != nil {
+			return errors.WithMessage(err, "calling Withdraw")
 		}
-
-		if err := reg.Timeout.Wait(ctx); err != nil {
-			return errors.WithMessage(err, "waiting for timeout")
+	case c.IsSubChannel():
+		if c.hasLockedFunds() {
+			return errors.New("cannot settle off-chain with locked funds")
 		}
+		if err := c.withdrawIntoParent(ctx); err != nil {
+			return errors.WithMessage(err, "withdrawing into parent channel")
+		}
+	default:
+		panic("invalid channel type")
 	}
 
-	if err := c.withdraw(ctx, secondary); err != nil {
-		return errors.WithMessage(err, "withdrawing")
+	if err := c.machine.SetWithdrawn(ctx); err != nil {
+		return errors.WithMessage(err, "setting machine phase")
 	}
-	c.Log().Info("Withdrawal successful.")
-	c.wallet.DecrementUsage(c.machine.Account().Address())
+
 	return nil
 }
 
@@ -189,22 +182,4 @@ func (c *Channel) register(ctx context.Context) error {
 	}
 
 	return c.machine.SetRegistered(ctx, reg)
-}
-
-// withdraw calls Withdraw on the adjudicator with the current channel state and
-// progresses the machine phases.
-//
-// The caller is expected to have locked the channel mutex.
-func (c *Channel) withdraw(ctx context.Context, secondary bool) error {
-	if err := c.machine.SetWithdrawing(ctx); err != nil {
-		return err
-	}
-
-	req := c.machine.AdjudicatorReq()
-	req.Secondary = secondary
-	if err := c.adjudicator.Withdraw(ctx, req); err != nil {
-		return errors.WithMessage(err, "calling Withdraw")
-	}
-
-	return c.machine.SetWithdrawn(ctx)
 }
