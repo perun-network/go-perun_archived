948e1fac73520d93098f06a14c1277d2f19c1628
Matthias Geihs <matthias@perun.network>
2020-11-18 09:41:42 +0100

    :construction_worker: [bindings] Revise generator script

--- backend/ethereum/bindings/generate.sh
+++ backend/ethereum/bindings/generate.sh
@@ -16,38 +16,48 @@
 
 set -e
 
+ABIGEN=abigen
+
 # Download solc.
-wget -nc "https://github.com/ethereum/solidity/releases/download/v0.7.4/solc-static-linux"
-chmod +x solc-static-linux
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # GNU Linux
+    wget -nc "https://github.com/ethereum/solidity/releases/download/v0.7.4/solc-static-linux"
+    chmod +x solc-static-linux
+    SOLC=./solc-static-linux
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # Mac OSX
+    curl -L "https://github.com/ethereum/solidity/releases/download/v0.7.4/solc-macos" -o solc-macos
+    chmod +x solc-macos
+    SOLC=./solc-macos
+
+else
+    # Unsupported
+    echo "$OSTYPE unsupported. Exiting."
+    exit 1
+fi
+
 echo -e "Exec 'git submodule update --init --recursive' once after cloning."
 echo -e "Ensure that the newest version of abigen is installed"
 
 # Generates optimized golang bindings and runtime binaries for sol contracts.
 # $1  solidity file path, relative to ../contracts/contracts/.
-# $1  golang package name.
-# $2… list of contract names.
+# $2  golang package name.
 function generate() {
-    file=$1; pkg=$2
-    shift; shift   # skip the first two args.
-    for contract in "$@"; do
-        abigen --pkg $pkg --sol ../contracts/contracts/$file.sol --out $pkg/$file.go --solc ./solc-static-linux
-        ./solc-static-linux --bin-runtime --optimize --allow-paths *, ../contracts/contracts/$file.sol --overwrite -o $pkg/
-        echo -e "package $pkg\n\n // ${contract}BinRuntime is the runtime part of the compiled bytecode used for deploying new contracts.\nvar ${contract}BinRuntime = \`$(<${pkg}/${contract}.bin-runtime)\`" > "$pkg/${contract}BinRuntime.go"
-    done
+    FILE=$1; PKG=$2; CONTRACT=$FILE
+    echo "generate package $PKG"
+    abigen --pkg $PKG --sol ../contracts/contracts/$FILE.sol --out $PKG/$FILE.go --solc $SOLC
+    $SOLC --bin-runtime --optimize --allow-paths *, ../contracts/contracts/$FILE.sol --overwrite -o $PKG/
+    echo -e "package $PKG\n\n // ${CONTRACT}BinRuntime is the runtime part of the compiled bytecode used for deploying new contracts.\nvar ${CONTRACT}BinRuntime = \`$(<${PKG}/${CONTRACT}.bin-runtime)\`" > "$PKG/${CONTRACT}BinRuntime.go"
 }
 
 # Adjudicator
 generate "Adjudicator" "adjudicator" "Adjudicator"
 
 # PerunToken, AssetHolderETH and AssetHolderERC20
-# Pragma statements can only be in a solidity file once, so
-# we remove the duplicates with awk.
-cat ../contracts/contracts/PerunToken.sol \
-    <(awk '/^pragma/{p=1;next}{if(p){print}}' ../contracts/contracts/AssetHolderETH.sol) \
-    <(awk '/^pragma/{p=1;next}{if(p){print}}' ../contracts/contracts/AssetHolderERC20.sol) \
-    > ../contracts/contracts/Contracts.sol
-generate "Contracts" "assets" "PerunToken" "AssetHolderETH" "AssetHolderERC20"
-rm ../contracts/contracts/Contracts.sol
+generate "PerunToken" "assets"
+generate "AssetHolderETH" "assets"
+generate "AssetHolderERC20" "assets"
 
-abigen --version --solc ./solc-static-linux
+$ABIGEN --version
 echo -e "Generated bindings"
